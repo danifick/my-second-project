b3e09abd1c0c66f9e78b88b1cfb66ad414b5f2ac
danifick <danifick24@outlook.com>
2026-08-21 17:39:17 +0300

    Оживил проценты

---
+++ script.js
@@ -0,0 +1,36 @@
+const billInput = document.querySelector('#bill-input')
+const peopleNum = document.querySelector('#people-input')
+const customInput = document.querySelector('.custom__input')
+const tipBtn = document.querySelectorAll('.tip-btn')
+const resBtn = document.querySelector('.btn__reset')
+const Amount = document.querySelector('.tip-amount-result')
+const Total = document.querySelector('.total-result')
+
+let billValue = 0;
+let peopleValue = 0;
+let tipValue = 0
+
+
+billInput.addEventListener('input', function() {
+
+billValue = Number(billInput.value)
+if (peopleValue > 0) {
+    Total.textContent = `$${(billValue / peopleValue).toFixed(2)}`
+}
+})
+
+peopleNum.addEventListener('input', function() {
+peopleValue = Number(peopleNum.value)
+if(peopleValue > 0) {
+    Total.textContent = `$${(billValue / peopleValue).toFixed(2)}`
+}
+})
+
+tipBtn.forEach(function(btn) {
+    btn.addEventListener('click', function() {
+     tipValue = parseFloat(btn.textContent)
+     if(peopleValue > 0) {
+        Amount.textContent = `$${(billValue * tipValue / 100 / peopleValue).toFixed(2)}`
+     }
+    })
+})
