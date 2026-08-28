b2920d55da15984f5eea73ee7d726d2c2eba980a
danifick <danifick24@outlook.com>
2026-08-28 19:51:24 +0300

    Пофиксил все баги (вроде)

--- script.js
+++ script.js
@@ -15,14 +15,18 @@ billInput.addEventListener('input', function() {
 
 billValue = Number(billInput.value)
 if (peopleValue > 0) {
-    Total.textContent = `$${(billValue / peopleValue).toFixed(2)}`
+  const checkPerPerson = billValue / peopleValue
+  const tipPerPerson = (billValue * tipValue / 100) / peopleValue  
+  Total.textContent = `$${(checkPerPerson + tipPerPerson).toFixed(2)}`
 }
 })
 
 peopleNum.addEventListener('input', function() {
 peopleValue = Number(peopleNum.value)
 if(peopleValue > 0) {
-    Total.textContent = `$${(billValue / peopleValue).toFixed(2)}`
+ const checkPerPerson = billValue / peopleValue
+ const tipPerPerson = (billValue * tipValue / 100) / peopleValue  
+ Total.textContent = `$${(checkPerPerson + tipPerPerson).toFixed(2)}`
 }
 })
 
@@ -35,6 +39,9 @@ tipBtn.forEach(function(btn) {
      tipValue = parseFloat(btn.textContent)
      if(peopleValue > 0) {
         Amount.textContent = `$${(billValue * tipValue / 100 / peopleValue).toFixed(2)}`
+        const checkPerPerson = billValue / peopleValue
+        const tipPerPerson = (billValue * tipValue / 100) / peopleValue 
+        Total.textContent = `$${(checkPerPerson + tipPerPerson).toFixed(2)}`
      }
     })
 })
@@ -50,6 +57,10 @@ customInput.addEventListener ('input', function() {
   }
    if(peopleValue > 0) {
         Amount.textContent = `$${(billValue * tipValue / peopleValue).toFixed(2)}`
+
+        const checkPerPerson = billValue / peopleValue
+        const tipPerPerson = (billValue * tipValue) / peopleValue 
+        Total.textContent = `$${(checkPerPerson + tipPerPerson).toFixed(2)}`
      }
 })
 
@@ -64,4 +75,22 @@ tipValue = 0
 
 Amount.textContent = "$0.00"
 Total.textContent = "$0.00"
+
+
+tipBtn.forEach(function(d) {
+  d.classList.remove("active")
+}
+)
+})
+
+customInput.addEventListener('focus', function() {
+tipBtn.forEach(function(e) {
+  e.classList.remove("active")
+}
+)
+tipValue = 0
+if(peopleValue > 0 ) {
+  Amount.textContent = "$0.00"
+  Total.textContent = `$${(billValue / peopleValue).toFixed(2)}`
+}
 })
